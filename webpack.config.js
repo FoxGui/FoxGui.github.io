306eb8d0473ad1979ad71b1fd979e08f7456d312
FoxGui <1162183871@qq.com>
2019-05-19 22:48:55 +0800

    fixed(webpack): 'add hotModule'

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js'
+  },
   devtool: 'inline-source-map',
   // 告诉devServer从什么位置查找文件
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: '管理输出'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: "main.js",
