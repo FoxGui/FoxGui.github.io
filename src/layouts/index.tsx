8f98839b8fc10a13cf6cf847ddcf8b13f758111f
桂权 <1162183871@qq.com>
2020-04-07 00:56:00 +0800

    feat(layout): 'add header'

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import styles from './index.css';
+import styles from './index.less';
+
+import Header from './header';
 
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      <Header />
+      {/* <h1 className={styles.title}>Yay! Welcome to umi!</h1> */}
       {props.children}
     </div>
   );
